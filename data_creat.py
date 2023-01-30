6ed2e5c9b0accce1a0500908e00415217dc3a913
Alsu <shakilsu@yandex.ru>
2023-01-30 11:49:06 +0300

    dz

---
+++ data_creat.py
@@ -0,0 +1,17 @@
+# Здесь мы создали функцию ввода данных
+
+def name_data():
+    name = input('Ваше имя: ')
+    return name
+
+def surname_data():
+    surname = input('Ваша фамилия: ')
+    return surname
+
+def phone_data():
+    phone = input('Ваш телефон: ')
+    return phone
+
+def address_data():
+    address = input('Ваш адрес: ')
+    return address
